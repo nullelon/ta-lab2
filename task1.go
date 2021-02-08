0cda7feae3f620f0da0ea8380e4a98864c0de1de
nullelon <nullelon@gmail.com>
2021-02-09 01:15:01 +0200

    algorithms implement

--- task1.go
+++ task1.go
@@ -1,9 +1,23 @@
 package lab2
 
 func ReverseDigitsSequence(sequence string) string {
-	panic("implement me")
+	if sequence[0] == '0' {
+		return sequence
+	}
+	for i, c := range sequence {
+		if c == '0' {
+			return ReverseDigitsSequence(sequence[1:i] + "0" + string(sequence[0]) + sequence[i+1:])
+		}
+	}
+	panic("wrong input")
 }
 
 func TaskTwoFunction(m, n int) int {
-	panic("implement me")
+	if m == 0 || m == n {
+		return 1
+	}
+	if 0 < m && m < n {
+		return TaskTwoFunction(m, n-1) + TaskTwoFunction(m-1, n-1)
+	}
+	panic("wrong input")
 }
