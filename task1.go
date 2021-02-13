7f4aad37f6f125ddb078841bb4926dfc976bd8cb
nullelon <nullelon@gmail.com>
2021-02-13 14:10:29 +0200

    update tests

--- task1.go
+++ task1.go
@@ -1,23 +1,38 @@
 package lab2
 
-func ReverseDigitsSequence(sequence string) string {
+type IReverse interface {
+	ReversedNums(sequence string) string
+}
+
+type RecursiveReversion struct{}
+type IteratedReversion struct{}
+
+func (r RecursiveReversion) ReversedNums(sequence string) string {
 	if sequence[0] == '0' {
 		return sequence
 	}
-	for i, c := range sequence {
-		if c == '0' {
-			return ReverseDigitsSequence(sequence[1:i] + "0" + string(sequence[0]) + sequence[i+1:])
+	for i, char := range sequence {
+		if char == '0' {
+			return r.ReversedNums(sequence[:i-1] + "0" + sequence[i+1:] + string(sequence[i-1]))
 		}
 	}
-	panic("wrong input")
+	panic("wrong input, '0' not found")
 }
 
-func TaskTwoFunction(m, n int) int {
+func (IteratedReversion) ReversedNums(sequence string) string {
+	var reversed string
+	for _, v := range sequence {
+		reversed = string(v) + reversed
+	}
+	return reversed
+}
+
+func C(m int, n int) int {
+	if !(0 < m || m < n) {
+		panic("wrong input")
+	}
 	if m == 0 || m == n {
 		return 1
 	}
-	if 0 < m && m < n {
-		return TaskTwoFunction(m, n-1) + TaskTwoFunction(m-1, n-1)
-	}
-	panic("wrong input")
+	return C(m, n-1) + C(m-1, n-1)
 }
--- task1_test.go
+++ task1_test.go
@@ -2,18 +2,51 @@ package lab2
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
-func TestReverseDigitsSequence(t *testing.T) {
-	data := map[string]string{
-		"12340":  "04321",
-		"5550":   "0555",
-		"8760":   "0678",
-		"976240": "042679",
+var reversionData = map[string]string{
+	"12340":  "04321",
+	"5550":   "0555",
+	"8760":   "0678",
+	"976240": "042679",
+}
+
+func TestRecursiveReversion_ReversedNums(t *testing.T) {
+	a := RecursiveReversion{}
+	for input, r := range reversionData {
+		result := a.ReversedNums(input)
+		if result != r {
+			t.Fatalf("Got %s, expected %s\n", result, r)
+		}
+		fmt.Printf("%s -> %s\n", input, result)
 	}
-	for input, r := range data {
-		result := ReverseDigitsSequence(input)
+}
+
+func TestIteratedReversion_ReversedNums(t *testing.T) {
+	a := IteratedReversion{}
+	for input, r := range reversionData {
+		result := a.ReversedNums(input)
+		if result != r {
+			t.Fatalf("Got %s, expected %s\n", result, r)
+		}
+		fmt.Printf("%s -> %s\n", input, result)
+	}
+}
+
+func TestRandomImplementation(t *testing.T) {
+	var reverse IReverse
+	if rand.Intn(2) == 0 {
+		reverse = RecursiveReversion{}
+		fmt.Println("Using RecursiveReversion implementation...")
+	} else {
+		reverse = IteratedReversion{}
+		fmt.Println("Using IteratedReversion implementation...")
+	}
+
+	for input, r := range reversionData {
+		result := reverse.ReversedNums(input)
 		if result != r {
 			t.Fatalf("Got %s, expected %s\n", result, r)
 		}
@@ -22,10 +55,15 @@ func TestReverseDigitsSequence(t *testing.T) {
 }
 
 func TestTaskTwoFunction(t *testing.T) {
-	m := []int{1, 2, 3, 4, 7}
+	m := []int{2, 2, 3, 4, 5}
 	n := []int{4, 20, 15, 40, 50}
+	result := []int{6, 190, 455, 91390, 2118760}
 
 	for i := range m {
-		fmt.Printf("C(%d, %d) = %d\n", m[i], n[i], TaskTwoFunction(m[i], n[i]))
+		r := C(m[i], n[i])
+		if r != result[i] {
+			t.Fatalf("Expected %d, got %d", result[i], r)
+		}
+		fmt.Printf("C(%d, %d) = %d\n", m[i], n[i], r)
 	}
 }
